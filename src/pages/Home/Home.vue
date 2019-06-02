8d5d940d6c253fae68103bd4b7444b4cfac430f9
OnePercents <1353212179@qq.com>
2019-06-02 12:04:23 +0800

    travel-demo

---
+++ src/pages/Home/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="Home">
+        <home-header></home-header>
+    </div>
+</template>
+
+<script>
+import homeHeader from './Header'
+export default {
+    name:'Home',
+    components:{
+        homeHeader
+    }
+}
+</script>
+
+<style>
+
+</style>
+
+
